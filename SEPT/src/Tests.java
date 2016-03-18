1062de2c441b25272525e01e20971fc8654e3848
Richard Liu <richard1421@msn.com>
2016-03-18 17:48:18 +1100

    Test commit and added JUnit test file

---
+++ SEPT/src/Tests.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public final class Tests
+{
+    // tests that one equals one
+    @Test
+    public void testSample()
+    {
+        assertEquals(1, 1);
+    }
+}
