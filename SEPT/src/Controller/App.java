4b12c874a878bd0dd1350edabec97a7ae006fceb
Quan Do <quan.dhz@gmail.com>
2016-03-19 16:46:58 +1100

    Add App skeleton code

---
+++ SEPT/src/Controller/App.java
@@ -0,0 +1,22 @@
+package Controller;
+
+public class App {
+	
+	public static void addToFavourites(String code) {
+		
+	}
+
+	public static void removeFromFavourites(String code) {
+		
+	}
+	
+	public static void setCurrentState(int state) {
+		// save current state to file/DB
+		// restore when reopen
+	}
+	
+	public static int getCurrentState() {
+		return 0;
+	}
+	
+}
